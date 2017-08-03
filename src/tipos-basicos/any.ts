56bc05d843e935124ce841f8d2ad217e1eaa166d
victorcuervo <vcuervo@gmail.com>
2017-08-04 00:48:26 +0200

    Variables de tipo Any

---
+++ src/tipos-basicos/any.ts
@@ -0,0 +1,11 @@
+// Uso de una variable de tipo Any
+// Aprovecha el dinamismo de Javascript con el tipado de TypeScript
+
+let x:any = 3;
+console.log(x);
+
+x = 'soy una cadena de texto';
+console.log(x);
+
+x = true;
+console.log(x);
