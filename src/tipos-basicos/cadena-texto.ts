b1593536c573ee27ed5a521c3667fabd919d1b2e
victorcuervo <vcuervo@gmail.com>
2017-08-03 22:30:24 +0200

    Cadenas de texto en TypeScript

---
+++ src/tipos-basicos/cadena-texto.ts
@@ -0,0 +1,12 @@
+// Cadenas definidas en TypeScript
+
+let saludo:string = "Hola Mundo";
+let nombre:string = 'Carmen';
+
+console.log(nombre);
+
+// Ejemplo de cadena con plantilla
+let edad:number = 39;
+let mensaje:string = `El próximo mes cumplo ${edad+1} años`;
+
+console.log(mensaje);
