a7782a9f324a578b34450f611085e6c322c1be73
victorcuervo <vcuervo@gmail.com>
2017-08-03 22:30:34 +0200

    Números en TypeScript

---
+++ src/tipos-basicos/numero.ts
@@ -0,0 +1,11 @@
+// Ejemplo de tipo básico numérico
+
+let decimal:number = 9;
+let hexadecimal:number = 0xf00d;
+let octal:number = 0o744;
+let binario:number = 0b1110;
+
+console.log(decimal);
+console.log(hexadecimal);
+console.log(octal);
+console.log(binario);
