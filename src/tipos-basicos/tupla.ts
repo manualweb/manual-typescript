45d037001cca457ab16b5f0b4c7e8241ad9696e9
victorcuervo <vcuervo@gmail.com>
2017-08-04 00:35:48 +0200

    Crear tuplas en TypeScript

---
+++ src/tipos-basicos/tupla.ts
@@ -0,0 +1,13 @@
+//Definir tuplas en TypeScript
+
+let tupla: [string, number];
+tupla = ['España',10];
+
+
+// Inserciones en la tupla que no fallan
+tupla[2] = 3;
+tupla[3] = 'Hola';
+tupla[4] = 4;
+
+console.log(tupla[0]);
+console.log(tupla);
