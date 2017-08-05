72c6d12b23aff8b13c502fb834ea9b1f6cad0d69
victorcuervo <vcuervo@gmail.com>
2017-08-06 01:25:44 +0200

    Swapping de variables

---
+++ src/variables/swapping-variables.ts
@@ -0,0 +1,14 @@
+// Swapping de variables
+
+namespace Swapping {
+
+  let numeros:number[] = [1,2];
+  let [x,y] = numeros;
+
+  // Hacemos el Swapping
+  [x,y] = [y,x];
+
+  console.log(x);
+  console.log(y);
+
+}
