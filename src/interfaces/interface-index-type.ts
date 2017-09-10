65604df3159b936a46f9f9d8b80579334f763726
Víctor Cuervo <vcuervo@gmail.com>
2017-09-10 19:49:49 +0200

    Interface para arrays

---
+++ src/interfaces/interface-index-type.ts
@@ -0,0 +1,11 @@
+// Interface que define un índice
+
+interface ArrayCadenas {
+  [index:number]: string;
+}
+
+let nombres:ArrayCadenas;
+nombres = ['Víctor','Nacho','Marta'];
+
+let n1:string = nombres[1];
+console.log(nombre);
