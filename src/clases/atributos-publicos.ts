66d7cedcd46921bbe9cbf029ffcbca1d8f2a00ee
Víctor Cuervo <vcuervo@gmail.com>
2017-09-12 01:04:06 +0200

    atributos públicos

---
+++ src/clases/atributos-publicos.ts
@@ -0,0 +1,18 @@
+
+namespace AtributosPublicos {
+
+  class Cuadrado {
+    public lado:number;
+    constructor(lado:number) {
+      this.lado = lado;
+    }
+    public calcularArea(){
+      return this.lado*2;
+    }
+  }
+
+  let c = new Cuadrado(3);
+  console.log(c.lado);
+  console.log(c.calcularArea());
+
+}
