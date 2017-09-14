ee67f98d7c69a6ceeb3adc9e0aee0525e7cc0831
Víctor Cuervo <vcuervo@gmail.com>
2017-09-14 18:01:09 +0200

    Clase abstracta en TypeScript

---
+++ src/clases/clase-abstracta.ts
@@ -0,0 +1,28 @@
+// Definir una clase abstracta
+
+
+namespace ClaseAbstracta {
+
+  abstract class Figura {
+    protected area:number;
+    getArea():number {
+      return this.area;
+    }
+  }
+
+  class Cuadrado extends Figura {
+    private lado:number;
+    constructor(lado:number) {
+      super();
+      this.lado = lado;
+      this.area = this.lado*2;
+    }
+  }
+
+  let c1 = new Cuadrado(2);
+  console.log(c1.getArea());
+
+  // No se puede instanciar una clase abstracta
+  // let f1 = new Figura();
+
+}
